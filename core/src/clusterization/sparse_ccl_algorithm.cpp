66ec8c132a4ab25e52a0178f979d1407d21d46c2
s1952531 <s1952531@phcomputeppe01.ph.ed.ac.uk>
2024-11-06 13:49:19 +0000

    Added CSV write of cluster info

--- core/src/clusterization/sparse_ccl_algorithm.cpp
+++ core/src/clusterization/sparse_ccl_algorithm.cpp
@@ -20,14 +20,16 @@
 
 //io for investigation
 #include <iostream>
+#include <filesystem>
+#include <fstream>
 
 namespace traccc::host {
 
-sparse_ccl_algorithm::sparse_ccl_algorithm(vecmem::memory_resource& mr)
-    : m_mr(mr) {}
+    sparse_ccl_algorithm::sparse_ccl_algorithm(vecmem::memory_resource& mr)
+        : m_mr(mr) {}
 
-sparse_ccl_algorithm::output_type sparse_ccl_algorithm::operator()(
-    const edm::silicon_cell_collection::const_view& cells_view) const {
+    sparse_ccl_algorithm::output_type sparse_ccl_algorithm::operator()(
+        const edm::silicon_cell_collection::const_view& cells_view) const {
 
     std::cout << "calling ca()" << std::endl;
 
@@ -50,17 +52,62 @@ sparse_ccl_algorithm::output_type sparse_ccl_algorithm::operator()(
 
     std::cout << "\t" << "Creating the result container" << std::endl;
     // Create the result container.
-    output_type clusters{m_mr.get()};
+    output_type clusters{m_mr.get()}; //output type is edm::silicon_cluster
     clusters.resize(num_clusters);
 
     std::cout << "\t" << "Adding cells to their clusters" << std::endl;
+    std::cout << "\t" << "Number of triggered cells: " << cluster_indices.size() << std::endl;
+    std::cout << "\t" << "Number of clusters: " << num_clusters << std::endl;
     // Add cells to their clusters.
     for (unsigned int cell_idx = 0; cell_idx < cluster_indices.size();
          ++cell_idx) {
         clusters.cell_indices()[cluster_indices[cell_idx]].push_back(cell_idx);
+
+
+        //if (cell_idx < 2){
+        // std::cout << "\t" << "Adding cell " << cell_idx << " to cluster " << cluster_indices[cell_idx] << std::endl;
+        // std::cout << "\t" << "Cell " << cell_idx << " has channel0 " << cells.channel0()[cell_idx] << std::endl;
+        // std::cout << "\t" << "Cell " << cell_idx << " has channel1 " << cells.channel1()[cell_idx] << std::endl;
+        // std::cout << "\t" << "Cell " << cell_idx << " has activation " << cells.activation()[cell_idx] << std::endl;
+        // std::cout << "\t" << "Cell " << cell_idx << " has time " << cells.time()[cell_idx] << std::endl;
+        // std::cout << "\t" << "Cell " << cell_idx << " has module_index " << cells.module_index()[cell_idx] << std::endl;
+
+        std::filesystem::path file_path;
+        // Write cluster data to respective event file
+        //size_t event_idx = 0;
+        // do {
+        //     file_path = "ClusterizationData/event" + std::to_string(event_idx) + ".csv";
+        //     event_idx++;
+        // } while (std::filesystem::exists(file_path));  // Increment until a non-existing file is found
+        file_path = "ClusterizationData/test.csv";
+
+        // Open the file for writing
+        std::ofstream myfile;
+        myfile.open(file_path, std::ios_base::app);
+
+        // If the file is empty, write the header
+        if (std::filesystem::file_size(file_path) == 0) {
+            myfile << "cluster,module_index,channel0,channel1\n";
+        }
+
+        // Write the data
+        myfile << cluster_indices[cell_idx] << ","
+            << cells.module_index()[cell_idx] << "," 
+            << cells.channel0()[cell_idx] << "," 
+            << cells.channel1()[cell_idx] << "\n";
+
+        myfile.close();
+
+        //}      
+        
     }
 
+    std::cout << cluster_indices.size() << std::endl;
+
+    std::cout << clusters.cell_indices().size() << std::endl;
+    
     std::cout << "\t" << "Returning the clusters" << std::endl;
+    
     // Return the clusters.
     return clusters;
 }
